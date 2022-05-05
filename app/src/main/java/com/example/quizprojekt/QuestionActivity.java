f1a3649599d6808c9d49ecc98188cd80a26539ed
Hubert <hubertos120@gmail.com>
2022-05-05 22:31:23 +0200

    changing activity

--- app/src/main/java/com/example/quizprojekt/QuestionActivity.java
+++ app/src/main/java/com/example/quizprojekt/QuestionActivity.java
@@ -6,17 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class QuestionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_question);
 
         Button button = findViewById(R.id.button);
 
         button.setOnClickListener(v -> {
-            Intent intent = new Intent(this, QuestionActivity.class);
+            Intent intent = new Intent(this, FinishActivity.class);
             startActivity(intent);
         });
     }
