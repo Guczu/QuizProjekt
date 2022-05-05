9776f0695759717aa81fecabfd0b284970293a6a
Guczu <jakub180720@gmail.com>
2022-05-05 18:24:00 +0200

    layout switching

--- app/src/main/java/com/example/quizprojekt/MainActivity.java
+++ app/src/main/java/com/example/quizprojekt/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.quizprojekt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        button1.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.button1:
+                setContentView(R.layout.activity_question);
+                break;
+            case R.id.button2:
+                setContentView(R.layout.activity_finish);
+                break;
+        }
+    }
+
 }
